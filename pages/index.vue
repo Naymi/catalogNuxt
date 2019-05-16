ae06f595310a846e9811d7cc3da24c9d45ae7663
Anatoly <tolikrut@gmail.com>
2019-05-16 17:23:05 +0300

    remove trash

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,7 @@
 <template>
-  <section class="container"></section>
+  <section class="container">
+    <nuxt-link to="/catalog">
+      Каталог
+    </nuxt-link>
+  </section>
 </template>
-
-<style>
-.container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  min-height: 100vh;
-  margin: 0 auto;
-
-  text-align: center;
-}
-
-.title {
-  display: block;
-
-  letter-spacing: 1px;
-
-  color: #35495e;
-
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 100px;
-  font-weight: 300;
-}
-
-.subtitle {
-  padding-bottom: 15px;
-
-  word-spacing: 5px;
-
-  color: #526488;
-
-  font-size: 42px;
-  font-weight: 300;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
